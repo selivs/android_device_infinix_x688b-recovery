0c27b3f6ab0ac716141eb99e9e7c44b30017b665
selivs <112254307+selivs@users.noreply.github.com>
2022-08-27 21:50:59 +0700

    Update omni_Infinix.mk

--- omni_Infinix.mk
+++ omni_Infinix.mk
@@ -2,4 +2,4 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/Infinix/Infinix-X688B/full_Infinix.mk)
 
-PRODUCT_NAME := omni_Infinix
+PRODUCT_NAME := omni_Infinix-X688B
