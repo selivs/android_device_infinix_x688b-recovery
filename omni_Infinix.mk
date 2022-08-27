bc2a025d49488f2610cd81c6f1a839e0021d1fcf
selivs <112254307+selivs@users.noreply.github.com>
2022-08-27 21:14:32 +0700

    Update and rename omni_Infinix-X688B.mk to omni_Infinix.mk

--- omni_Infinix.mk
+++ omni_Infinix.mk
@@ -2,4 +2,4 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/Infinix/Infinix-X688B/full_Infinix-X688B.mk)
 
-PRODUCT_NAME := omni_Infinix-X688B
+PRODUCT_NAME := omni_Infinix
