2cde2644e106b158ba47ef0af581bb85c0b6892d
lopestorm <guillopes2002@gmail.com>
2022-08-29 10:21:08 -0300

    hide lines

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo omni_Infinix-X688B-userdebug
-add_lunch_combo omni_Infinix-X688B-eng
+#add_lunch_combo omni_Infinix-X688B-userdebug
+#add_lunch_combo omni_Infinix-X688B-eng
